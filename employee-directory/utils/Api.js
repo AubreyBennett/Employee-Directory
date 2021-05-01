759a478e67b5250c3f00e50571d78fdd3c7804d6
Aubrey Bennett <27aubreyb@gmail.com>
2021-04-30 21:06:49 -0400

    added api

--- employee-directory/utils/Api.js
+++ employee-directory/utils/Api.js
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getRandomEmployee: function() {
+    return axios.get("https://randomuser.me/api/");
+  }
+};
